42fb0d0768ae7334e7d5b4fb27f3553b29833742
Luis Daniel <ldcastil@bancoagricola.com.sv>
2022-11-02 18:48:11 -0600

    Primero

---
+++ msvc-usuarios/src/test/java/org/mcastillo/springcloud/msvc/usuarios/msvcusuarios/MsvcUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mcastillo.springcloud.msvc.usuarios.msvcusuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
